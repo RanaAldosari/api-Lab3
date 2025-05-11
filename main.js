d1c38c6762ce38e7771bbec357c4153dee102fb1
Rana Aldosari <rana@example.com>
2025-05-11 13:55:56 +0300

    update

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ const resCodes = [
 let container=document.getElementById("container")
 for(let i of resCodes){
     let div=document.createElement("div")
+   
     let img=document.createElement("img")
     img.src=`https://http.cat/${i}`
     img.style.width="200px"
